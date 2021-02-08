60a86eb56793f237aef76bce8f47bc01738c6149
Kriskooo <kris.danielov@gmail.com>
2021-02-08 23:58:44 +0200

    RegularExpressionsLab-Exercise

---
+++ RegularExpressionsLab-ExerciseSeptember/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RegularExpressionsLab_ExerciseSeptember
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
